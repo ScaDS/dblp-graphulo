bafeb844d0204381ef7794fd396be169e3713b41
Werkelmann <al-kern@web.de>
2017-05-18 14:08:29 +0200

    fixed errors in graphulo script

--- graphulo.sh
+++ graphulo.sh
@@ -2,16 +2,20 @@
 
 cd ~/installs
 touch test.sh
+echo "#!/bin/bash" >> test.sh
+echo "cd graphulo" >> test.sh
 echo "mvn test -DTEST_CONFIG=./example.conf -Dtest=TableMultExample" >> test.sh
+chmod +x test.sh
 git clone https://github.com/Accla/graphulo.git
 sudo apt-get install maven
+
 cd graphulo
-mvn package -DskipTests
-deploy.sh
 rm example.conf
 touch example.conf
-echo "accumulo.it.cluster.standalone.admin.principal=username" >> example.conf
+echo "accumulo.it.cluster.standalone.admin.principal=root" >> example.conf
 echo "accumulo.it.cluster.standalone.admin.password=acc" >> example.conf
 echo "accumulo.it.cluster.standalone.zookeepers=localhost:2181" >> example.conf
 echo "accumulo.it.cluster.standalone.instance.name=bdp" >> example.conf
+mvn install -DskipTests
+./deploy.sh
 
