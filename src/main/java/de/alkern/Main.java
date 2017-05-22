7dfaae24eea34b75a0f8db317076dfd35c6ec494
alkern <al-kern@web.de>
2017-05-22 13:13:07 +0200

    fixes for presentation

--- src/main/java/de/alkern/Main.java
+++ src/main/java/de/alkern/Main.java
@@ -1,28 +1,26 @@
 package de.alkern;
 
 import de.alkern.author.AuthorProcessor;
-import de.alkern.graphulo.examples.MultiplyWithValueOp;
+import de.alkern.graphulo.GraphuloConnector;
+import de.alkern.infrastructure.ExampleData;
+import de.alkern.infrastructure.GraphuloProcessor;
+import de.alkern.infrastructure.connector.AccumuloConnector;
 import de.alkern.infrastructure.repository.AdjacencyRepository;
 import de.alkern.infrastructure.repository.Repository;
-import de.alkern.infrastructure.ExampleData;
-import de.alkern.infrastructure.connector.LocalConnector;
-import org.apache.accumulo.core.client.AccumuloException;
-import org.apache.accumulo.core.client.AccumuloSecurityException;
-import org.apache.accumulo.core.client.TableExistsException;
-import org.apache.accumulo.core.client.TableNotFoundException;
-
-import java.io.IOException;
+import edu.mit.ll.graphulo.Graphulo;
+import org.apache.accumulo.core.client.*;
 
 public class Main {
 
     public static void main(String[] args)
             throws AccumuloSecurityException, AccumuloException, TableExistsException, TableNotFoundException {
-        Repository repo = new AdjacencyRepository("authors", new LocalConnector().get());
-        AuthorProcessor processor = new AuthorProcessor(repo);
+        Connector conn = AccumuloConnector.local();
+        Repository repo = new AdjacencyRepository("authors", conn);
+        GraphuloProcessor processor = new AuthorProcessor(repo);
         processor.parse(ExampleData.EXAMPLE_DATA);
         processor.scan().forEach(System.out::println);
-        MultiplyWithValueOp mwv = new MultiplyWithValueOp();
-        mwv.use(processor.getIterator()).forEach(System.out::println);
-        processor.clear();
+        Graphulo graphulo = GraphuloConnector.local(conn);
+        graphulo.generateDegreeTable("authors", "authors_deg", true);
+        //processor.clear();
     }
 }
--- src/main/java/de/alkern/infrastructure/connector/AccumuloConnector.java
+++ src/main/java/de/alkern/infrastructure/connector/AccumuloConnector.java
@@ -4,11 +4,14 @@ import org.apache.accumulo.core.client.*;
 import org.apache.accumulo.core.client.security.tokens.PasswordToken;
 
 public abstract class AccumuloConnector {
-    public abstract Connector get() throws AccumuloSecurityException, AccumuloException;
 
     static Connector getConnector(String instanceName, String zookeepers, String user, String password)
             throws AccumuloSecurityException, AccumuloException {
         Instance instance = new ZooKeeperInstance(instanceName, zookeepers);
         return instance.getConnector(user, new PasswordToken(password));
+    }
+
+    public static Connector local() throws AccumuloSecurityException, AccumuloException {
+        return getConnector("bdp", "localhost:2181", "root", "acc");
     }
 }
