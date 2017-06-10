d65da18b8159d10fe46723fd393ed4ceb51c7de9
alkern <al-kern@web.de>
2017-06-10 17:51:07 +0200

    better tests

--- src/test/java/de/alkern/graphulo/connected_components/ConnectedComponentsTest.java
+++ src/test/java/de/alkern/graphulo/connected_components/ConnectedComponentsTest.java
@@ -8,15 +8,24 @@ import de.alkern.infrastructure.connector.AccumuloConnector;
 import de.alkern.infrastructure.entry.AdjacencyEntry;
 import de.alkern.infrastructure.repository.Repository;
 import de.alkern.infrastructure.repository.RepositoryImpl;
+import edu.mit.ll.graphulo.DynamicIteratorSetting;
 import edu.mit.ll.graphulo.Graphulo;
-import org.apache.accumulo.core.client.AccumuloException;
-import org.apache.accumulo.core.client.AccumuloSecurityException;
-import org.apache.accumulo.core.client.Connector;
+import edu.mit.ll.graphulo.skvi.CountAllIterator;
+import edu.mit.ll.graphulo.skvi.DynamicIterator;
+import org.apache.accumulo.core.client.*;
 import org.apache.accumulo.core.client.admin.TableOperations;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Range;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.iterators.LongCombiner;
+import org.apache.accumulo.core.iterators.system.CountingIterator;
+import org.apache.accumulo.core.security.Authorizations;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -31,7 +40,9 @@ public class ConnectedComponentsTest {
         Connector conn = AccumuloConnector.local();
         graphulo = GraphuloConnector.local(conn);
         operations = conn.tableOperations();
-//        fillDatabase(conn);
+        if (!(operations.exists("test") || operations.exists("test_deg"))) {
+           fillDatabase(conn);
+        }
     }
 
     private static void fillDatabase(Connector conn) throws Exception {
@@ -45,16 +56,30 @@ public class ConnectedComponentsTest {
     public void find() throws Exception {
         //splitConnectedComponents connected components
         new ConnectedComponents(graphulo).splitConnectedComponents( "test", "test_deg");
-//        TableOperations operations = conn.tableOperations();
-//        assertTrue(operations.exists("test_cc1"));
-//        assertTrue(operations.exists("test_cc2"));
-//        assertFalse(operations.exists("test_cc3"));
+        assertTrue(operations.exists("test_cc1"));
+        assertTrue(operations.exists("test_cc2"));
+        assertFalse(operations.exists("test_cc3"));
+
+        assertEquals(6, countEntries("test_cc1"));
+        assertEquals(2, countEntries("test_cc2"));
 
         //clean up
 //        operations.delete("test");
 //        operations.delete("test_deg");
-//        operations.delete("test_cc1");
-//        operations.delete("test_cc2");
+        operations.delete("test_cc1");
+        operations.delete("test_cc2");
+    }
+
+    private long countEntries(String table) throws TableNotFoundException {
+        BatchScanner scanner = graphulo.getConnector().createBatchScanner(table, Authorizations.EMPTY, 5);
+        scanner.setRanges(Collections.singleton(new Range()));
+        scanner.addScanIterator(new IteratorSetting(1, CountAllIterator.class));
+        long cnt = 0;
+        for (Map.Entry<Key, Value> entry : scanner) {
+            cnt += LongCombiner.STRING_ENCODER.decode(entry.getValue().get());
+        }
+        scanner.close();
+        return cnt;
     }
 
     @Test
